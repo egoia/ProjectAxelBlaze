d02ceeaa7ce97a88f509407922e78c06588636c2
egoia <elyas.saidoune@gmail.com>
2025-12-12 13:03:50 +0100

    started moving

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed; 
+    public float steering_speed;
+
+    Rigidbody rigidbody;
+    InputSystem_Actions inputActions;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inputActions = new InputSystem_Actions();
+        inputActions.Player.Enable();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float turnInput =  inputActions.Player.Turn.ReadValue<float>();
+        Quaternion rotation = Quaternion.Euler(0, turnInput*steering_speed*Time.deltaTime,0);
+        rigidbody.MoveRotation(rigidbody.rotation * rotation);
+
+        float moveInput = inputActions.Player.Move.ReadValue<float>();
+        Vector3 movement = transform.forward * speed * moveInput;
+        rigidbody.linearVelocity = new Vector3(movement.x, rigidbody.linearVelocity.y, movement.z);
+
+    }
+}
